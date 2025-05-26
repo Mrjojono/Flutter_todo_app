459ed32a72c788383c4a0734289f603e7e4ff56a
joan <nutsukpuijoan@gmail.com>
2025-05-26 16:54:25 +0200

    create Drawerclass

--- test/lib/components/DrawerComponent.dart
+++ test/lib/components/DrawerComponent.dart
@@ -11,58 +11,64 @@ class DrawerComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-          child: ListView(
-            padding: EdgeInsets.all(10),
-            children:[
+      child: ListView(
+        padding: EdgeInsets.all(10),
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(),
+            accountName: Text("username"),
+            accountEmail: Text("email"),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.lightBlue, Colors.deepPurpleAccent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
 
-              UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(),
-                accountName: Text("username"),
-                accountEmail: Text("email"),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.lightBlue, Colors.deepPurpleAccent],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                ),
-              ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Accueil"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
 
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Accueil"),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-
-              ListTile(
-                leading: Icon(Icons.task),
-                title: Text("Task"),
-                onTap: (){
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context)=> Task(),));
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.contact_page),
-                title: Text("Contact"),
-                onTap: (){
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context)=> const Home(),));
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text("Logout"),
-                onTap: (){
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) =>const Login()),//navigation vers la page login pour deconnexion
-                  );
-                },
-              ),
-            ],
-          )
+          ListTile(
+            leading: Icon(Icons.task),
+            title: Text("Task"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Task()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.contact_page),
+            title: Text("Contact"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Home()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Logout"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Login(),
+                ), //navigation vers la page login pour deconnexion
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
